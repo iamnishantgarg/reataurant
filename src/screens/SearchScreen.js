35d4df6c0977310f56fb31e7652bcdd3a9646b4a
Nishant Garg <nishant@nishants-MacBook-Air.local>
2021-09-19 13:43:30 +0530

    query api call done

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,35 @@
 import React, {useState} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import SearchBar from '../components/SearchBar'
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const onTermSubmit = () => {
-    console.log('submitted');
+  const [results, setResults] = useState([]);
+  const [errMessage, setErrMessage] = useState("");
+
+  const searchApi = async () => {
+    console.log(term);
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      console.log(err); 
+      setErrMessage('SOmething went wrong');
+    }
   }
+
   return (
     <View style={styles.background}>
-      <SearchBar term={term} onTermChange={(newTerm)=>setTerm(newTerm)} onTermSubmit={onTermSubmit}/>
-      <Text>{term}</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi}/>
+      {errMessage ? <Text>{errMessage}</Text>:null}
+      <Text>we have found : {results.length}</Text>
     </View>
   )
 }
